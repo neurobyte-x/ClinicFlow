4be60d6b0060684dd50488a864ff37692f2dd77e
Sooraj Hegde <soorajhegde557@gmail.com>
2026-05-20 01:59:19 +0530

    first commit

---
+++ src/main/java/com/neurobyte/clinicflow/ClinicflowApplication.java
@@ -0,0 +1,13 @@
+package com.neurobyte.clinicflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neurobyte/clinicflow/ClinicflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neurobyte.clinicflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
